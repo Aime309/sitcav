27bae43fa7ba6f48678bd3ed5c8d0c48fd071da5
Michell Manzanillo <luxiamber@gmail.com>
2024-12-22 14:35:24 -0400

    Cambiado logo y nombre de la app

--- src/views/pages/Login.svelte
+++
@@ -1,6 +0,0 @@
-<form method="post">
-  <h2>Login</h2>
-  <input type="email" name="email" value="example@mail.com" />
-  <input type="password" name="password" value="12345678" />
-  <button>Login</button>
-</form>
