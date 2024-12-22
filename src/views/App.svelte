8207d6cc89c6aefb43ca23d7ffdd6024fceb6815
Michell Manzanillo <luxiamber@gmail.com>
2024-12-22 15:47:36 -0400

    Instalada la plantilla de login

---
+++ src/views/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Route } from "svelte-routing";
+  import VeltRouter from "./VeltRouter.svelte";
+  import Login from "./pages/Login.svelte";
+
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "./styles/style.css";
+  import "./styles/responsive.css";
+  import "./styles/jquery.mCustomScrollbar.min.css";
+  import "fancybox/dist/css/jquery.fancybox.css";
+</script>
+
+<VeltRouter>
+  <Route path="/inicio">
+    <Login />
+  </Route>
+</VeltRouter>
--- src/views/layouts/base.php
+++ src/views/layouts/base.php
@@ -8,6 +8,7 @@
   <base href="<?= str_replace('index.php', '', $_SERVER['SCRIPT_NAME']) ?>" />
   <link rel="icon" href="./images/favicon.png" />
   <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <link rel="stylesheet" href="./build/bundle.css">
   <script defer src="./build/bundle.js"></script>
 </head>
