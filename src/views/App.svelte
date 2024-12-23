f464ec8dfdbe044c8befc190b5d7fa79cb69cc66
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-23 10:06:52 -0400

    Reformateado el archivo Login.svelte

--- src/views/App.svelte
+++ src/views/App.svelte
@@ -2,12 +2,6 @@
   import { Link, Route } from "svelte-routing";
   import VeltRouter from "./VeltRouter.svelte";
   import Login from "./pages/Login.svelte";
-
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import "./styles/style.css";
-  import "./styles/responsive.css";
-  import "./styles/jquery.mCustomScrollbar.min.css";
-  import "fancybox/dist/css/jquery.fancybox.css";
 </script>
 
 <VeltRouter>
