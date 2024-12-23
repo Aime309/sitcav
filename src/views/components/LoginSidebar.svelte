84cecb6368a90b67db13079e15343e993f0391b7
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-23 12:04:05 -0400

    Extraídas las secciones del login en componentes

---
+++ src/views/components/LoginSidebar.svelte
@@ -0,0 +1,15 @@
+<script>
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+</script>
+
+<div id="mySidepanel" class="sidepanel">
+  <a href="javascript:void(0)" class="closebtn" onclick={closeNav}>×</a>
+  <a href="index.html">Home </a>
+  <a href="#about">About</a>
+  <a href="#service">Services </a>
+  <a href="#vehicles">Our Vehicles</a>
+  <a href="#testimonial">Testimonial</a>
+  <a href="#contact">Contact</a>
+</div>
