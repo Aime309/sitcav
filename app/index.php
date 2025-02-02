17e20c5c8ae3950eb55a1df1cf5efcac1ce34ab5
Michell Manzanillo <luxiamber@gmail.com>
2025-02-02 13:20:11 -0400

    Extraídas las configuraciones

--- app/index.php
+++ app/index.php
@@ -1,58 +1,6 @@
 <?php
 
-use Illuminate\Container\Container;
-use Illuminate\Database\Capsule\Manager;
-use Symfony\Component\Dotenv\Dotenv;
-
-/////////////////////////
-// CONSTANTES GLOBALES //
-/////////////////////////
-/** Por ejemplo sería: `C:\xampp\htdocs\sitcav` */
-const CARPETA_RAIZ = __DIR__ . '/..';
-
-require CARPETA_RAIZ . '/vendor/autoload.php';
-
-////////////////////////////////////////////////////
-// CARGAR VARIABLES DE ENTORNO - ver archivo .env //
-////////////////////////////////////////////////////
-(new Dotenv)->load(CARPETA_RAIZ . '/.env');
-
-////////////////////////////////////////////////////
-// CONFIGURAR LEAF AUTH (módulo de autenticación) //
-////////////////////////////////////////////////////
-auth()->config('session', true);
-auth()->config('db.table', 'usuarios');
-auth()->config('password.key', 'clave');
-auth()->config('messages.loginParamsError', 'Cédula o contraseña incorrecta');
-auth()->config('messages.loginPasswordError', auth()->config('messages.loginParamsError'));
-auth()->config('timestamps', false);
-
-//////////////////////////////////////////////
-// CONFIGURAR MOTOR DE PLANTILLAS DE FLIGHT //
-//////////////////////////////////////////////
-Flight::set('flight.views.path', CARPETA_RAIZ . '/src');
-
-///////////////////////////////////////////
-// CONFIGURAR CONTENEDOR DE DEPENDENCIAS //
-///////////////////////////////////////////
-$container = new Container;
-$container->singleton(PDO::class, static fn(): PDO => db()->connection());
-
-/////////////////////////
-// CONFIGURAR ELOQUENT //
-/////////////////////////
-$manager = new Manager;
-
-$manager->addConnection([
-  'driver' => $_ENV['DB_CONNECTION'],
-  'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
-  'database' => $_ENV['DB_DATABASE'],
-  'username' => $_ENV['DB_USERNAME'] ?? 'root',
-  'password' => $_ENV['DB_PASSWORD'] ?? ''
-]);
-
-$manager->setAsGlobal();
-$manager->bootEloquent();
+require_once __DIR__ . '/configuraciones.php';
 
 //////////////////
 // CARGAR RUTAS //
