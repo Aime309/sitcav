c20c466f60b5087633e069fbcd0068f82e826f6e
fadrian06 <franyeradriansanchez@gmail.com>
2025-08-17 01:29:10 -0400

    Simplificado el header de la landing

--- frontend/src/componentes/landing/Navbar.svelte
+++ frontend/src/componentes/landing/Navbar.svelte
@@ -2,112 +2,101 @@
   import logo from "@/recursos/logo-texto.png";
 </script>
 
-<nav
-  class="navbar navbar-expand-lg text-white text-uppercase fs-7 ls-1 py-5 align-items-center"
->
-  <div class="container-fluid">
-    <div class="row align-items-center w-100">
-      <div class="col-8 col-md-3">
-        <a class="navbar-brand" href="./">
-          <img src={logo.src} height="46" alt="logo" style:filter="invert(1)" />
-        </a>
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-xl text-uppercase py-5">
+    <a href="./">
+      <img src={logo.src} height="46" alt="logo" style:filter="invert(1)" />
+    </a>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title">Menu</h5>
         <button
-          class="navbar-toggler"
           type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-label="Toggle navigation"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
         >
-          <span class="navbar-toggler-icon"></span>
         </button>
       </div>
-      <div class="col-1 col-md-6 d-md-flex justify-content-center">
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title">Menu</h5>
-            <button
-              type="button"
-              class="btn-close text-reset"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            >
-            </button>
-          </div>
 
-          <div class="offcanvas-body">
-            <ul
-              class="navbar-nav justify-content-end flex-grow-1 gap-1 gap-md-5 pe-3"
-            >
-              <li class="nav-item">
-                <a class="nav-link active" href="./">Inicio</a>
-              </li>
-              <!-- <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  data-bs-toggle="dropdown"
-                >
-                  Pages
-                </a>
-                <ul class="dropdown-menu list-unstyled p-4">
-                  <li>
-                    <a
-                      href="shop.html"
-                      class="dropdown-item d-flex justify-content-between align-items-center"
-                    >
-                      Shop
-                      <span class="badge bg-primary ms-2">PRO</span>
-                    </a>
-                  </li>
-                </ul>
-              </li> -->
-              <li class="nav-item">
-                <a href="./ingresar" class="btn btn-outline-light rounded-pill">
-                  Ingresar
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  href="./crear-cuenta"
-                  class="btn btn-outline-light rounded-pill"
-                >
-                  Crear cuenta
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-3 col-md-3 text-md-end">
-        <ul class="list-unstyled d-flex justify-content-end m-0">
-          <!-- <li>
-            <a href="wishlist.html" class="mx-2" aria-label="Lista de deseos">
-              <svg width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#heart"></use>
-              </svg>
-            </a>
+      <div class="offcanvas-body">
+        <ul
+          class="navbar-nav justify-content-center align-items-center flex-grow-1 gap-5"
+        >
+          <li class="nav-item">
+            <a href="./" class="nav-link">Inicio</a>
+          </li>
+          <!-- <li class="nav-item">
+            <div class="dropdown">
+              <a
+                href="#dropdown"
+                class="nav-link dropdown-toggle text-center"
+                data-bs-toggle="dropdown"
+              >
+                Pages
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a
+                    href="./shop"
+                    class="dropdown-item d-flex justify-content-between align-items-center"
+                  >
+                    Shop
+                    <span class="badge text-bg-primary">PRO</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </li> -->
-          <!-- <li>
-            <a
-              href="#"
-              class="mx-2"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasCart"
-            >
-              <svg width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#cart"></use>
-              </svg>
+          <li class="nav-item">
+            <a href="./ingresar" class="btn btn-outline-light rounded-pill">
+              Ingresar
             </a>
-          </li> -->
-          <!-- <li class="mx-2">
-            <a href="#search" class="search-button">
-              <svg width="24" height="24" viewBox="0 0 24 24">
-                <use xlink:href="#search"></use>
-              </svg>
+          </li>
+          <li class="nav-item">
+            <a href="./crear-cuenta" class="btn btn-outline-light rounded-pill">
+              Crear cuenta
             </a>
-          </li> -->
+          </li>
         </ul>
       </div>
     </div>
-  </div>
-</nav>
+    <ul class="navbar-nav flex-row align-items-center gap-2">
+      <!-- <li>
+        <a href="wishlist.html" aria-label="Lista de deseos">
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <use xlink:href="#heart"></use>
+          </svg>
+        </a>
+      </li> -->
+      <!-- <li>
+        <a
+          href="#cart"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasCart"
+          aria-label="Mi carrito"
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <use xlink:href="#cart"></use>
+          </svg>
+        </a>
+      </li> -->
+      <!-- <li>
+        <a href="#search" class="search-button" aria-label="Buscar">
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <use xlink:href="#search"></use>
+          </svg>
+        </a>
+      </li> -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </ul>
+  </nav>
+</div>
