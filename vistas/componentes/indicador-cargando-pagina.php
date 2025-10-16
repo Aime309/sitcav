a0ef3fc5b42932fdf8ab6fb71ae021b9e2d66a97
fadrian06 <franyeradriansanchez@gmail.com>
2025-10-16 18:42:26 -0400

    Corregido el indicador de carga

--- vistas/componentes/indicador-cargando-pagina.php
+++ vistas/componentes/indicador-cargando-pagina.php
@@ -1,6 +1,6 @@
 <div
   class="loader loader-bouncing"
-  :class="active && 'is-active'"
   x-data="{ active: true, }"
-  x-init="document.addEventListener('DOMContentLoaded', () => (this.active = false))">
+  x-init="setTimeout(() => { active = false }, 500)"
+  :class="active && 'is-active'">
 </div>
