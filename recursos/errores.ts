67df18c0340f8d110cdfc09e23ecfabed43f1d0c
fadrian06 <franyeradriansanchez@gmail.com>
2025-11-07 10:50:50 -0400

    Eliminado CSS innecesario

--- recursos/errores.ts
+++ recursos/errores.ts
@@ -1,3 +1,5 @@
+import "@fontsource/inter/latin.css";
+
 import Alpine from "alpinejs";
 import "./ts/tema";
 
--- vistas/diseños/materialm-para-errores.php
+++ vistas/diseños/materialm-para-errores.php
@@ -12,6 +12,7 @@
   <title><?= $titulo ?> | SITCAV</title>
   <base href="<?= str_replace('index.php', '', $_SERVER['SCRIPT_NAME']) ?>" />
   <link rel="icon" href="./recursos/imagenes/favicon.png" />
+  <link rel="stylesheet" href="./recursos/compilados/errores.css" />
   <link rel="stylesheet" href="./recursos/css/materialm.css" />
   <script src="./recursos/compilados/errores.js"></script>
 </head>
