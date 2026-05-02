0ab22fb47562a30fbd7285d7e6aac6f56e5db6c1
fadrian06 <franyeradriansanchez@gmail.com>
2026-05-02 01:10:49 -0400

    Restructure views: move dashboard and ecommerce to dedicated subdirectories

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 declare(strict_types=1);
 
-Flight::route('GET /', static fn() => Flight::render('pages/index'));
-Flight::route('GET /dashboard', static fn() => Flight::render('pages/index'));
+Flight::route('GET /', static fn() => Flight::render('pages/ecommerce/index'));
+Flight::route('GET /dashboard', static fn() => Flight::render('pages/dashboard/index'));
