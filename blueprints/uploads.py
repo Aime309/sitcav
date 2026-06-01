9383e444fd323ab13f2d369d6eb64ee6817e9e24
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-01 14:28:41 -0400

    Añadido tipado a uploads.py

--- blueprints/uploads.py
+++ blueprints/uploads.py
@@ -1,20 +1,22 @@
+from typing import cast
+
 from flask import Blueprint, current_app, send_from_directory
 from markupsafe import escape
 
-uploads_bp = Blueprint(name="uploads", import_name=__name__, url_prefix="/uploads")
+uploads_bp = Blueprint(
+    name="uploads", import_name=__name__, url_prefix="/uploads"
+)
 
 
 @uploads_bp.get("/productos/<path:filename>")
 def select_product_image(filename: str):
-    upload_folder = current_app.config["PRODUCTS_UPLOAD_FOLDER"]
+    directory = cast(str, current_app.config["PRODUCTS_UPLOAD_FOLDER"])
 
-
-    return send_from_directory(upload_folder, escape(filename))
+    return send_from_directory(directory, escape(filename))
 
 
 @uploads_bp.get("/profiles/<path:filename>")
 def select_user_photo(filename: str):
-    profiles_folder = current_app.config["PROFILE_UPLOAD_FOLDER"]
+    directory = cast(str, current_app.config["PROFILE_UPLOAD_FOLDER"])
 
-
-    return send_from_directory(profiles_folder, escape(filename))
+    return send_from_directory(directory, escape(filename))
