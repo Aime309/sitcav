a6ed37bd7bf11d68eb5ea787211d5c127a773aea
fadrian06 <franyeradriansanchez@gmail.com>
2026-06-29 14:12:24 -0400

    Creado el Api\ProductsController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Api\ProductsController;
 use Leaf\Exception\Handler\JsonResponseHandler;
 use Leaf\Exception\Run;
 
@@ -20,99 +21,17 @@ Flight::group(
       Flight::json(auth()->errors(), 401);
     });
 
-    Flight::route("GET /products/types", static function (): void {
-      Flight::json(
-        array_filter(
-          array_map(
-            static fn(array $product): string => $product["type"],
-            db()->select("products", "DISTINCT type")->all(),
-          ),
-        ),
-      );
-    })->setAlias("api.products.types");
-
-    Flight::route("GET /products/categories", static function (): void {
-      Flight::json(
-        array_filter(
-          array_map(
-            static fn(array $product): string => $product["category"],
-            db()->select("products", "DISTINCT category")->all(),
-          ),
-        ),
-      );
-    })->setAlias("api.products.categories");
-
-    Flight::route("GET /products", static function (): void {
-      Flight::json(
-        array_map(
-          static fn(array $product): array => [
-            "sources" => json_decode($product["sources"]),
-            "pinned" => (bool) $product["pinned"],
-          ] + $product,
-          db()->select("products")->all(),
-        ),
-      );
-    })->setAlias("api.products");
-
-    Flight::route("GET /products/pinned", static function (): void {
-      Flight::json(
-        array_map(
-          static fn(array $product): array => [
-            "src" => json_decode($product["sources"])[0],
-            "pinned" => (bool) $product["pinned"],
-          ] + $product,
-          db()->select("products")->where("pinned", true)->all(),
-        ),
-      );
-    })->setAlias("api.products.pinned");
-
-    Flight::route("GET /products/trending", static function (): void {
-      Flight::json([
-        [
-          "src" => "./img/tranding_item/tranding_item_1.png",
-          "name" => "Cervical pillow for airplane car office nap pillow",
-          "price" => 5,
-        ],
-        [
-          "src" => "./img/tranding_item/tranding_item_2.png",
-          "name" => "Foam filling cotton slow rebound pillows",
-          "price" => 5,
-        ],
-        [
-          "src" => "./img/tranding_item/tranding_item_3.png",
-          "name" => "Memory foam filling cotton Slow rebound pillows",
-          "price" => 5,
-        ],
-        [
-          "src" => "./img/tranding_item/tranding_item_4.png",
-          "name" => "Cervical pillow for airplane car office nap pillow",
-          "price" => 5,
-        ],
-        [
-          "src" => "./img/tranding_item/tranding_item_5.png",
-          "name" => "Foam filling cotton slow rebound pillows",
-          "price" => 5,
-        ],
-        [
-          "src" => "./img/tranding_item/tranding_item_6.png",
-          "name" => "Memory foam filling cotton Slow rebound pillows",
-          "price" => 5,
-        ],
-      ]);
-    })->setAlias("api.products.trending");
-
-    Flight::route("GET /products/@" . auth()->config("id.key"), static function (string $id): void {
-      $product = db()->select("products")->find($id);
-      $product["sources"] = json_decode($product["sources"]);
-      $product["stock"] = 10;
-
-      Flight::json($product);
-    })->setAlias("api.products.@" . auth()->config("id.key"));
+    Flight::router()->get('/products/types', [ProductsController::class, 'indexTypes'])->setAlias('api.products.types');
+    Flight::router()->get('/products/categories', [ProductsController::class, 'indexCategories'])->setAlias('api.products.categories');
+    Flight::router()->get('/products', [ProductsController::class, 'index'])->setAlias('api.products');
+    Flight::router()->get('/products/pinned', [ProductsController::class, 'indexPinned'])->setAlias('api.products.pinned');
+    Flight::router()->get('/products/trending', [ProductsController::class, 'indexTrending'])->setAlias('api.products.trending');
+    Flight::router()->get('/products/@id', [ProductsController::class, 'show'])->setAlias('api.products.@id');
   },
   [
     static function (): void {
-      $run = new Run();
-      $run->pushHandler(new JsonResponseHandler())->register();
+      $run = new Run;
+      $run->pushHandler(new JsonResponseHandler)->register();
     },
   ],
 );
