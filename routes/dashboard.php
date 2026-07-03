1abfc7f174c7dc648097f7bf25d9377e3141d049
fadrian06 <franyeradriansanchez@gmail.com>
2026-07-03 12:15:27 -0400

    Creado el Dashboard\IndexController

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Dashboard\IndexController;
 use App\Http\Middleware\Dashboard\Authenticate;
 use App\Http\Middleware\Dashboard\Authorize;
 use App\Http\Middleware\Dashboard\RedirectIfAuthenticated;
@@ -11,26 +12,57 @@ use Leaf\Exception\Run;
 Flight::group(
   '/dashboard',
   static function (): void {
-    Flight::router()->get('/login', static function (): void {
-      Flight::render('index');
-    })
-      ->addMiddleware(RedirectIfAuthenticated::class)
-      ->setAlias('dashboard.login');
+    Flight::group('/login', static function (): void {
+      Flight::router()->get('/', [IndexController::class, '__invoke'])->setAlias('dashboard.login');
 
-    Flight::router()->get('/register', static function (): void {
-      Flight::render('index');
-    })
-      ->addMiddleware(RedirectIfAuthenticated::class)
-      ->setAlias('dashboard.register');
+      Flight::post('/', static function (): void {
+        $data = Flight::request()->data;
+
+        if ($data['remember']) {
+          auth()->config('session.lifetime', 0);
+        }
+
+        if (auth()->login([
+          'email' => $data['email'],
+          auth()->config('password.key') => $data['password'],
+        ])) {
+          Flight::redirect(Flight::getUrl('dashboard.index'));
+
+          return;
+        }
+
+        Flight::redirect(Flight::getUrl("dashboard.login"));
+      });
+    }, [RedirectIfAuthenticated::class]);
+
+    Flight::group('/register', static function (): void {
+      Flight::router()->get('/', [IndexController::class, '__invoke'])->setAlias('dashboard.register');
+
+      Flight::post('/', static function (): void {
+        $data = Flight::request()->data->getData();
+
+        if (auth()->register([
+          auth()->config('id.key') => uniqid(),
+          'name' => $data['name'],
+          'email' => $data['email'],
+          auth()->config('password.key') => $data['password'],
+          auth()->config('roles.key') => json_encode(['Administrator']),
+        ])) {
+          Flight::redirect(Flight::getUrl('dashboard.index'));
+
+          return;
+        }
+
+        Flight::redirect(Flight::getUrl('dashboard.register'));
+      });
+    }, [RedirectIfAuthenticated::class]);
 
     Flight::router()->get('/logout', static function (): void {
       auth()->logout();
       Flight::redirect(Flight::getUrl('dashboard.login'));
     })->setAlias('dashboard.logout');
 
-    Flight::router()->get('/*', static function (): void {
-      Flight::render('index');
-    })
+    Flight::router()->get('(/@page:.+)', [IndexController::class, '__invoke'])
       ->addMiddleware(Authenticate::class)
       ->addMiddleware(Authorize::class)
       ->setAlias('dashboard.index');
