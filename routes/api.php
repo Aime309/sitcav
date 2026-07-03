61f8f54bee25f3b6db43f351b5bdd2b2aebffbb9
fadrian06 <franyeradriansanchez@gmail.com>
2026-07-03 12:15:09 -0400

    Eliminado el login de la api

--- routes/api.php
+++ routes/api.php
@@ -9,18 +9,6 @@ use Leaf\Exception\Run;
 Flight::group(
   '/api',
   static function (): void {
-    Flight::route('POST /login', static function (): void {
-      $data = Flight::request()->data;
-
-      if (auth()->login(['email' => $data['email'], 'password' => $data['password']])) {
-        Flight::json(auth()->user()->getAuthInfo());
-
-        return;
-      }
-
-      Flight::json(auth()->errors(), 401);
-    });
-
     Flight::router()->get('/products/types', [ProductsController::class, 'indexTypes'])->setAlias('api.products.types');
     Flight::router()->get('/products/categories', [ProductsController::class, 'indexCategories'])->setAlias('api.products.categories');
     Flight::router()->get('/products', [ProductsController::class, 'index'])->setAlias('api.products');
